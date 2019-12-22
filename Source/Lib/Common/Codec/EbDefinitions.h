65bddc4d13bdc851d32984aa49eba9b7d45214bb
hguermaz <hassen.guermazi@intel.com>
2019-12-22 15:45:22 -0800

    shut SHUT_D1_D2_EARLY_EXIT

--- Source/Lib/Common/Codec/EbDefinitions.h
+++ Source/Lib/Common/Codec/EbDefinitions.h
@@ -55,7 +55,7 @@ extern "C" {
 #define IMPROVED_MULTI_PASS_PD       1
 #if IMPROVED_MULTI_PASS_PD
 #define ADD_SUPPORT_TO_SKIP_PART_N   1
-#define SHUT_D1_D2_EARLY_EXIT        1
+#define SHUT_D1_D2_EARLY_EXIT        0
 
 #define SHUT_SPLIT_COST              1
 #define POST_PD2_INTER_DEPTH         1
