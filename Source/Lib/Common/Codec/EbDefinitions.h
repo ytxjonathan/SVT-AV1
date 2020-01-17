6169409c827eb808655e0d56a358d9def4b9ad52
hguermaz <hassen.guermazi@intel.com>
2020-01-17 08:20:11 -0800

    Enabled FAST_RDOQ for INTER CHROMA

--- Source/Lib/Common/Codec/EbDefinitions.h
+++ Source/Lib/Common/Codec/EbDefinitions.h
@@ -146,9 +146,9 @@ extern "C" {
 #define ATB_INTER_2_DEPTH            1 // ATB INTRA Depth 2
 #define TX_ORG_INTERINTRA            1
 #define ENABLE_BC                    0 // ATB for BC
-#define FASTER_RDOQ                  0
+#define FASTER_RDOQ                  1
 #define UPGRAGDE_TX_WEIGHT           0 
-#define ADD_4TH_MD_STAGE             1
+#define ADD_4TH_MD_STAGE             0
 
 #define IMPROVED_MULTI_PASS_PD       0
 #if IMPROVED_MULTI_PASS_PD
