bdc4a794560c8256af426c3c3ee62f2cf8e5f318
PhoenixWorthVCD <phoenix.worth@intel.com>
2020-02-03 09:13:02 -0800

    quantize_fp for INTER blocks and Fast_RDOQ support
    Signed-off-by: PhoenixWorthVCD <phoenix.worth@intel.com>
    Signed-off-by: hguermaz <hassen.guermazi@intel.com>

--- Source/Lib/Common/Codec/EbDefinitions.h
+++ Source/Lib/Common/Codec/EbDefinitions.h
@@ -46,6 +46,9 @@ extern "C" {
 
 #define MR_MODE 0
 
+#define FASTER_RDOQ 1
+#define FP_QUANT_BOTH_INTRA_INTER 1
+
 #define HIGH_PRECISION_MV_QTHRESH 150
 // Actions in the second pass: Frame and SB QP assignment and temporal filtering strenght change
 //FOR DEBUGGING - Do not remove
